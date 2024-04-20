968bb4fce3d2a93dcad0ce23f0e6975650c90401
Peixoto07 <lucaspeixotodacruz@gmail.com>
2024-04-20 18:40:12 -0300

    cria filtro das casas

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -1,6 +1,30 @@
+import { Link } from 'react-router-dom'; 
+import useHouse from '../hook/useHouse';
+import House from './House/House';
+
 
 const HouseList = () => {
-  return <div>HouseList</div>;
+  const { casas, loading } = useHouse(); 
+
+  return (
+    <section className='mb-20'>
+      <div className='container mx-auto'>
+       
+        {loading ? (
+          <p>Carregando...</p>
+        ) : (
+          <div className='grid md:grid-cols-2 lg:grid-cols-3  gap-4'>
+        
+            {casas.map((casa, index) => (
+              <Link className='mb-8' to={`/property/${casa.id}`} key={index}>
+                <House casa={casa} />
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default HouseList;
--- src/context/HouseContext.jsx
+++ src/context/HouseContext.jsx
@@ -9,36 +9,52 @@ export const HouseContext = createContext();
 export const HouseContextProvider = ({ children }) => {
 
   const [casas, setCasas] = useState(housesData);
-  const [pais, setPais] = useState('Localização (qualquer)');
+  const [pais, setPais] = useState('Qualquer localização');
   const [paises, setPaises] = useState([]);
-  const [propriedade, setPropriedade] = useState('Propriedade (qualquer)');
+  const [propriedade, setPropriedade] = useState('Qualquer propiedade');
   const [propriedades, setPropriedades] = useState([]);
-  const [preco, setPreco] = useState('Faixa de preço (qualquer)');
+  const [preco, setPreco] = useState('Qualquer valor');
   const [precos, setPrecos] = useState([]);
   const [loading, setloading] = useState(false);
 
-  
+
   useEffect(() => {
     const todosPaises = casas.map((casa) => casa.country);
-  const paisesUnicos = ['Localização (qualquer)', ...new Set(todosPaises)];
+    const paisesUnicos = ['Qualquer localização', ...new Set(todosPaises)];
 
-  const todosTipos = casas.map((casa) => casa.type);
-  const tiposUnicos = ['Propriedade (qualquer)', ...new Set(todosTipos)];
+    const todosTipos = casas.map((casa) => casa.type);
+    const tiposUnicos = ['Qualquer propiedade', ...new Set(todosTipos)];
 
-  
-  const precosUnicos = faixaDePrecos.map(item => item.value);
 
-  
-  
-  setPrecos(precosUnicos);
-  setPaises(paisesUnicos);
-  setPropriedades(tiposUnicos);
-  
-}, []);
+    const precosUnicos = faixaDePrecos.map(item => item.value);
 
-const handleClick = ()=>{
-  console.log('clicou');
-};
+
+
+    setPrecos(precosUnicos);
+    setPaises(paisesUnicos);
+    setPropriedades(tiposUnicos);
+
+  }, []);
+
+  const handleClick = () => {
+    const precoMin = parseInt(preco.split(' ')[0]);
+    const precoMax = parseInt(preco.split(' ')[2]);
+  
+    const isQualquerValor = (valor) => valor.split(' ').includes('Qualquer');
+  
+    const filtraCasas = housesData.filter((casa) => {
+      const precoDasCasas = parseInt(casa.price);
+  
+      const atendeFiltroPais = isQualquerValor(pais) || casa.country === pais;
+      const atendeFiltroTipo = isQualquerValor(propriedade) || casa.type === propriedade;
+      const atendeFiltroPreco = isQualquerValor(preco) || (precoDasCasas >= precoMin && precoDasCasas <= precoMax);
+  
+      return atendeFiltroPais && atendeFiltroTipo && atendeFiltroPreco;
+    });
+  setCasas(filtraCasas);
+
+  };
+  
 
 const valores = {
   casas,
@@ -59,11 +75,11 @@ const valores = {
   setloading,
   handleClick,
 };
-  return (
-    <HouseContext.Provider value={valores}>
-      {children}
-    </HouseContext.Provider>
-  );
+return (
+  <HouseContext.Provider value={valores}>
+    {children}
+  </HouseContext.Provider>
+);
 };
 
 export default HouseContextProvider;

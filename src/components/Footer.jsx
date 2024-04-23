9fa4fc32cb97e321da5354b92854c2156c8e8d67
Peixoto07 <lucaspeixotodacruz@gmail.com>
2024-04-23 12:16:13 -0300

    cria footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-violet-800 text-white py-8">
+      <div className="container mx-auto flex justify-center">
+        <p className="text-sm">&copy; 2024 Todos os direitos reservados</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

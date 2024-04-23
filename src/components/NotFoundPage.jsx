70803db5389fc759436a7723f5a16710a8048a17
Peixoto07 <lucaspeixotodacruz@gmail.com>
2024-04-23 12:15:21 -0300

    cria page not found

---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import { BsHouseX } from "react-icons/bs";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex justify-center h-screen gap-4">
+      <BsHouseX className=" size-32 text-gray-200 " />
+      <div className="flex flex-col items-start">
+        <h1 className=" text-6xl font-bold text-gray-400 mb-2">404</h1>
+        <h2 className="text-xl font-bold text-gray-400 mb-2">Página não encontrada</h2>
+        <p className=" text-gray-400">Pesquise novamente</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;

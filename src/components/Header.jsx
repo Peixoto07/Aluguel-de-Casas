43e7e6e29ea77cbd9582610910aab40d51fa0f13
Peixoto07 <lucaspeixotodacruz@gmail.com>
2024-04-25 12:18:15 -0300

    ajusta responsividade do sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,8 +24,8 @@ const Header = () => {
 
   return (
 
-    <header className="py-4 transition mb-12 border-b fixed shadow-1 top-0 left-0 right-0 bg-white z-10">
-  <div className=" gap-8 container mx-auto grid sm:flex justify-center sm:justify-between items-center">
+    <header className="py-4 transition-all  border-b fixed shadow-1 top-0 left-0 right-0 bg-white z-10">
+  <div className=" container mx-auto grid sm:flex justify-center sm:justify-between items-center">
     <div className="flex justify-between w-[90vw]" >
     <Link to={"/"}>
       <img src={Logo} alt="" />
@@ -37,7 +37,7 @@ const Header = () => {
  
     </button>
     </div>
-    <div className={`${showMenu ? "" : "hidden"} transition-all sm:flex w-[90vw] sm:w-auto sm:mx-0  mx-auto text-center grid items-center gap-2`}>
+    <div className={`${showMenu ? " pt-8" : "h-0 overflow-hidden"} sm:overflow-visible sm:pt-0 transition-all sm:flex w-[90vw] sm:w-auto sm:mx-0  mx-auto text-center grid items-center gap-2`}>
       <Link onClick={handleSignInClick} className="hover:text-violet-900 px-4 py-3 rounded-lg border transition">Entrar</Link>
       <Link onClick={handleSignUpClick} className="bg-violet-700 hover:bg-violet-800 text-nowrap text-white px-4 py-3 rounded-lg transition">
         Cadastrar-se

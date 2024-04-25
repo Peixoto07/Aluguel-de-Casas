275de25ece43bc89230e6699330e9bebb6d92077
Peixoto07 <lucaspeixotodacruz@gmail.com>
2024-04-25 20:34:06 -0300

    ajusta responsividade

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -11,18 +11,18 @@ const HouseList = () => {
   const { casas, loading } = useHouse();
 
   return (
-    <section className=" mx-auto max-w-[1800px] sm:px-8 mb-20">
+    <section className=" mx-auto max-w-[1800px] py-8 xl:py-10 xxl:py-24">
 
       {loading ? (
-        <div  className="grid md:grid-cols-2 lg:grid-cols-4 gap-2">
-          <SkeletonLoader  count={4} />
+        <div  className="grid md:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4 gap-2">
+          <SkeletonLoader  count={8} />
         </div>
       ) : casas.length < 1 ? (
         <NotFoundPage />
       ) : (
-        <div className="flex flex-wrap ">
+        <div className="flex gap-y-4 flex-wrap ">
           {casas.map((casa, index) => (
-            <Link className="mb-4 px-2 xl:w-1/3 xxl:w-1/4 lg:w-1/3 md:w-1/2 w-full" to={`/property/${casa.id}`} key={index}>
+            <Link className="mb-4 px-3 xl:w-1/3 xxl:w-1/4 lg:w-1/3 md:w-1/2 w-full" to={`/property/${casa.id}`} key={index}>
               <House casa={casa} />
             </Link>
           ))}
